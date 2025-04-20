6fd58d7c6b37bdf81a82deb14ff36471c8034a44
Rizwana1239875 <142738234+Rizwana1239875@users.noreply.github.com>
2025-04-21 00:59:57 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+# Dictionaries are written with curly brackets,and have keys and values:
+# created and print a dictionary:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+# Dictionary items:
+# print the "brand" value of the dictionary:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
