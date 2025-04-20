6aadd423aee3cb1850f17292fd214cdafa007d4d
Rizwana1239875 <142738234+Rizwana1239875@users.noreply.github.com>
2025-04-21 00:47:47 +0500

    Add files via upload

---
+++ set.py
@@ -0,0 +1,25 @@
+thisset = {"apple","banana","cherry"}
+print(thisset)
+
+# duplicate values will be ignored:
+thisset = {"apple","banana","cherry","apple","cherry"}
+print(thisset)
+
+# get the no of items in a set:
+thisset = {"apple","banana","cherry"}
+print(len(thisset))
+
+# string, int and boolean data types:
+set1 = {"apple","banana","cherry"}
+set2 = {1,3,6,9,2}
+set3 = {True, False, False}
+
+# A set can contain different data types:
+# A set with strings,integer and boolean values:
+set1 = {"abc",34,True,45,"male"}
+print(set1)
+
+# what is the data type of a set?
+myset = {"apple","banana","cherry"}
+print(type(myset))
+ 
